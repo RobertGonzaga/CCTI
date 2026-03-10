ec82b03b7b053b526c454591731a7b42025bec3c
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-03-10 17:11:01 -0400

    refiz o template em javascript e iniciei a tarefa de react component

---
+++ MODULO 4/my-daily-habits/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <h1>My Daily Habits</h1>
+      <p>Gerencie seus hábitos diários</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ MODULO 4/my-daily-habits/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <small>Módulo 04 - Desenvolvimento Front-End com React - ITEAM</small>
+    </footer>
+  );
+}
+
+export default Footer;
