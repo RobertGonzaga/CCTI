beac55d21b9cd6f06132055cc36e4a42911dc1ed
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-05-20 16:03:42 -0400

    aula 03

---
+++ MODULO PYTHON/aula02/aula02.py
@@ -0,0 +1,8 @@
+# nome = input("Digite seu nome: ")
+# idade = input("Digite sua idade: ")
+
+a = 10
+b = a
+print(b)
+a = 40
+print(b)
---
+++ MODULO PYTHON/aula03/aula03.py
@@ -0,0 +1,9 @@
+dia = 20
+mes = "maio"
+ano = 2026
+
+dia_do_solicitante = input("Digite o seu dia: ")
+
+if dia_do_solicitante == dia :
+  print("Seja bem vindo, vamos coversar")
+  
